4875b7767f23c5442b994a7a32040b6c8356a852
tom-shimoda <shimoda0204@gmail.com>
2023-03-24 22:39:26 +0900

    Add shell script.

---
+++ scatter-dotfiles.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+set -e
+
+function copy_to() {
+    if [ -e $1 ]; then
+        echo "copy $1 to here."
+        cp $3 $1 $2
+    else
+        echo "$1 is not found."
+    fi
+}
+
+copy_to .zshrc ~
+copy_to .tmux.conf ~
+copy_to .tmux-pane-border ~
+copy_to .p10k.zsh ~
+copy_to .gitconfig ~
+copy_to config/nvim ~/.config -R
